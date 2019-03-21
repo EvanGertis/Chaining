f6cc66804285b5589efe55c2e485a027d5f6f58b
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-21 04:44:15 -0400

    added linked list initialization to Chaing class constructor

--- src/com/company/ChainedHashtable.java
+++ src/com/company/ChainedHashtable.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class ChainedHashtable {
+    private LinkedList<StoredEmployee>[] hashTable;
+
+    public ChainedHashtable(){
+        hashTable = new LinkedList[10];
+
+        for(int i = 0; i < hashTable.length; i++){
+            hashTable[i] = new LinkedList<StoredEmployee>();
+        }
+    }
+
+    //hashing function.
+    private int hashKey(String key){
+        return key.length() % hashTable.length;
+    }
 }
